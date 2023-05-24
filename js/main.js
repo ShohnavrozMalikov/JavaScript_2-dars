cf760b8e46cd2dafddb22ae3f1a3276be46a098e
ShohnavrozMalikov <133765496+ShohnavrozMalikov@users.noreply.github.com>
2023-05-24 13:07:01 +0500

    Bu JavaScript 2-dars kodlari bor
    Yuklandi

---
+++ js/main.js
@@ -0,0 +1,49 @@
+/*let a = 44;
+let b = 43;
+if (a == b){
+    console.log("a qiymati 44dan katta yoki teng!");
+}else{
+    console.log("a ning qiymati ko'rsatilgan sondan katta yoki teng emas!");
+}*/
+
+/*let a = 10;
+let b = 46;
+let c = 50;
+let d = 55;*/
+/*if (a == 10 && b == 46 && c == 50 && d == 55){
+    console.log("Shartlarni hammasi bajarildi");
+}
+else{
+    console.log("Shartlarning kamida bittasi ishlamayapti");
+}*/
+
+/*if (a == 10 || b == 46 || c == 50 || d == 55){
+    console.log("Shartlarning kamida bittasi bajarildi");
+}
+else{
+    console.log("Shartlardan hammasi bajarilmadi");
+}*/
+
+/* JS data types: undefined,boolean,object */
+/*let soz = "text";
+let son = 45;
+let matn;*/
+/*let rost = 45==44?true:false;*/
+/*if (45 == 45){
+    document.write(true);
+}
+else{
+    document.write(false);
+}*/
+
+let arrayObject = [45,78,"text","matn",true,false];
+
+let oddiyObject = {ismi:"Navruz", yoshi: 18, talabami: "yoq"};
+let talabami = "-Yo'q, talaba emas";
+if (oddiyObject.yoshi > 17){
+    talabami = "-Ha, talaba"
+}
+
+console.log("-Navruz talabami? " + talabami);
+
+
